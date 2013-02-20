92d9ce92ee93ebce5498a6245c6e5e41315ba27d
jfeng <jfengsky@gmail.com>
2013-02-20 14:29:27 +0800

    modify UIButtonTest

--- UIButtonTest/UIButtonTest/UIButtonTestViewController.m
+++ UIButtonTest/UIButtonTest/UIButtonTestViewController.m
@@ -19,6 +19,13 @@
     [super viewDidLoad];
     // 创建一个button
     UIButton *sampleButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    // 各种风格 UIButtonTypeDetailDisclosure UIButtonTypeInfoLight UIButtonTypeInfoDark
+    
+    // 设置背景图 forState 设置按钮状态
+    [sampleButton setImage:[UIImage imageNamed:@"pic"] forState:UIControlStateNormal];
+    
+    
+    
     
     // button位置
     [sampleButton setFrame:CGRectMake(10, 100, self.view.bounds.size.width- 20, 52)];
@@ -28,7 +35,15 @@
     
     // view中加入button
     [self.view addSubview:sampleButton];
+    
+    [sampleButton addTarget:self action:@selector(btnPressed:) forControlEvents:UIControlEventTouchUpInside];
 	
+}
+
+-(void)btnPressed:(id)sender
+{
+    //    UIButton *btn = (UIButton *)sender;
+    NSLog(@"btn Pressed");
 }
 
 
