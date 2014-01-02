1807ac27e31c9646d5d7c89b1e2c4e17c04a34a3
jfeng <jfengsky@gmail.com>
2014-01-02 22:24:17 +0800

    Initial Commit

---
+++ _study_ORD/1.4/circle/circle/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  circle
+//
+//  Created by 姜峰 on 14-1-2.
+//  Copyright (c) 2014年 姜峰. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
