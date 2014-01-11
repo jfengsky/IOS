46643081a6ba3cda1c8a9779217cff06139208d4
jfeng <jfengsky@gmail.com>
2014-01-11 14:15:11 +0800

    modify

--- _study_ORD/1.4/study/study/main.m
+++ _study_ORD/1.4/study/study/main.m
@@ -8,13 +8,33 @@
 
 #import <Foundation/Foundation.h>
 
+BOOL areIntsDifferent(int thing1, int thing2)
+{
+    if (thing1 == thing2) {
+        return (NO);
+    } else {
+        return (YES);
+    }
+}
+
+NSString *boolString(BOOL yesNo){
+    if (yesNo == NO) {
+        return (@"NO");
+    } else{
+        return (@"YES");
+    }
+}
+
 int main(int argc, const char * argv[])
 {
 
     @autoreleasepool {
         
-        // insert code here...
-        NSLog(@"Hello, World!");
+        BOOL areTheyDifferent;
+        
+        areTheyDifferent = areIntsDifferent(5, 5);
+        
+        NSLog(@"are %d and %d different? %@", 5, 5, boolString(areTheyDifferent));
         
     }
     return 0;
