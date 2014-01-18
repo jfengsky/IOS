78207fca105a0e6b60dec10e4536edc442cd885c
jfeng <jfengsky@gmail.com>
2014-01-18 13:18:44 +0800

    Initial Commit

---
+++ _study_ORD/1.4/Obcjective/Obcjective/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Obcjective
+//
+//  Created by 姜峰 on 14-1-18.
+//  Copyright (c) 2014年 姜峰. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
