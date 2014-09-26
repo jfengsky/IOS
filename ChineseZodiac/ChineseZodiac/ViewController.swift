300bb3143c35c7e5db7ba645c1d90c499e638d69
jfeng <jfengsky@gmail.com>
2014-09-26 21:59:21 +0800

    add ChineseZodiac

---
+++ ChineseZodiac/ChineseZodiac/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ChineseZodiac
+//
+//  Created by 姜峰 on 14-9-26.
+//  Copyright (c) 2014年 jfeng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
