252ecabd262d79b638372c8283b652ac14ae42a2
jfeng <jfengsky@gmail.com>
2014-10-09 21:53:23 +0800

    chinesezodiac

---
+++ ChineseZodiac/ChineseZodiac/MyPlayground.playground/section-1.swift
@@ -0,0 +1,7 @@
+// Playground - noun: a place where people can play
+
+import Cocoa
+
+var offset = 2008 % 12
+
+(2014 - offset) % 12
--- ChineseZodiac/ChineseZodiac/ViewController.swift
+++ ChineseZodiac/ChineseZodiac/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var yearOfBirth: UITextField!
+    
+    @IBOutlet weak var image: UIImageView!
+    
+    let offset = 4
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +25,19 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    override func touchesEnded(touches: NSSet, withEvent event: UIEvent) {
+        yearOfBirth.resignFirstResponder()
+    }
+    
+    @IBAction func okTapped(sender: AnyObject) {
+        yearOfBirth.resignFirstResponder()
+        
+        if let year = yearOfBirth.text.toInt() {
+            var imageNumber = (year - offset) % 12
+            image.image = UIImage(named: String(imageNumber))
+        }
+    }
 
 }
 
