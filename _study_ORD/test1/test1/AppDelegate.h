f4e1bf9badb8766e407f1b5d3ddb7957f4ea4a27
jfengsky <jfengsky@gmail.com>
2014-11-17 23:42:59 +0800

    Initial Commit

---
+++ _study_ORD/test1/test1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test1
+//
+//  Created by jiang feng on 14/11/17.
+//  Copyright (c) 2014年 jiang feng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ _study_ORD/test1/test1/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  test1
+//
+//  Created by jiang feng on 14/11/17.
+//  Copyright (c) 2014年 jiang feng. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
