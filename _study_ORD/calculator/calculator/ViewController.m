57fb506acce3f84a4d64a8ae777e2d52148ddac1
jfengsky <jfengsky@gmail.com>
2014-11-19 22:45:51 +0800

    add calculator

---
+++ _study_ORD/calculator/calculator/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  calculator
+//
+//  Created by jiang feng on 14/11/19.
+//  Copyright (c) 2014年 www.ieake.com. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ _study_ORD/calculator/calculator/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  calculator
+//
+//  Created by jiang feng on 14/11/19.
+//  Copyright (c) 2014年 www.ieake.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
