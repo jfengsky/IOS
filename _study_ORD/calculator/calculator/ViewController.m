28cb5d6cfa2476bb17f9d95e70a8555070c694d7
jfengsky <jfengsky@gmail.com>
2014-11-20 22:04:15 +0800

    calculator modify

--- _study_ORD/calculator/calculator/ViewController.m
+++ _study_ORD/calculator/calculator/ViewController.m
@@ -13,30 +13,73 @@
 @end
 
 @implementation ViewController
-
+    UILabel *resultLabel;
 - (void)viewDidLoad {
+    UIButton *Buttons;
+    NSString *nums;
     [super viewDidLoad];
     
     // 创建UILabel用于显示数字
-    UILabel *resultLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 20, 320, 60)];
-    resultLabel.text = @"0";
+    resultLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 20, 320, 60)];
+    resultLabel.text = @"1";
     resultLabel.font = [UIFont systemFontOfSize: 40];
     resultLabel.textAlignment = 2;
     resultLabel.textColor = [UIColor blackColor];
     resultLabel.backgroundColor = [UIColor grayColor];
+    int height = 60;
+    int width = 60;
+    int space = 10;
+    int x_distance = 10;
+    int y_distance = 80;
+    int step;
+    int ystep = 0;
+    for (int i = 0; i < 10; i++) {
+        
+        // int转化为NSString
+        nums = [[NSString alloc] initWithFormat:@"%d", i];
+        step = i % 3;
+        if (step == 0) {
+            x_distance = 10;
+            ystep++;
+        }
+        x_distance = space + (width + space) * step;
+        y_distance = space + (height + space) * ystep;
+        Buttons = [[UIButton alloc] initWithFrame:CGRectMake(x_distance, y_distance, height, width)];
+        [Buttons setTitle:nums forState:UIControlStateNormal];
+        Buttons.titleLabel.font = [UIFont systemFontOfSize: 40];
+        [Buttons setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+        Buttons.backgroundColor = [UIColor redColor];
+        [self.view addSubview:Buttons];
+        [Buttons addTarget:self action:@selector(ButtonEvent) forControlEvents:UIControlEventTouchUpInside];
+    }
     
-    // 创建数字按钮0
-    UIButton *Button0 = [[UIButton alloc] initWithFrame:CGRectMake(10, 80, 60, 60)];
-    [Button0 setTitle:@"0" forState:UIControlStateNormal];
-    Button0.titleLabel.font = [UIFont systemFontOfSize: 40];
-    // Button0.font = [UIFont fontWithName:@"Arial" size:40];
-    [Button0 setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
-    Button0.backgroundColor = [UIColor  redColor];
-    
+//    // 创建数字按钮0
+//    UIButton *Button0 = [[UIButton alloc] initWithFrame:CGRectMake(10, 80, height, width)];
+//    [Button0 setTitle:@"0" forState:UIControlStateNormal];
+//    Button0.titleLabel.font = [UIFont systemFontOfSize: 40];
+//    // Button0.font = [UIFont fontWithName:@"Arial" size:40];
+//    [Button0 setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+//    Button0.backgroundColor = [UIColor  redColor];
+//    
+//    // 创建数字按钮1
+//    UIButton *Button1 = [[UIButton alloc] initWithFrame:CGRectMake(80, 80, 60, 60)];
+//    [Button1 setTitle:@"1" forState:UIControlStateNormal];
+//    Button1.titleLabel.font = [UIFont systemFontOfSize: 40];
+//    // Button0.font = [UIFont fontWithName:@"Arial" size:40];
+//    [Button1 setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+//    Button1.backgroundColor = [UIColor  redColor];
+//    
+//    // 创建数字2
+//    UIButton *Button2 = [[UIButton alloc] initWithFrame:CGRectMake(150, 80, 60, 60)];
+//    [Button2 setTitle:@"2" forState:UIControlStateNormal];
+//    Button2.titleLabel.font = [UIFont systemFontOfSize: 40];
+//    // Button0.font = [UIFont fontWithName:@"Arial" size:40];
+//    [Button2 setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+//    Button2.backgroundColor = [UIColor  redColor];
     
     [self.view addSubview:resultLabel];
-    [self.view addSubview:Button0];
-    [Button0 addTarget:self action:@selector(Button0Event) forControlEvents:UIControlEventTouchUpInside];
+
+    
 }
 
 - (void)didReceiveMemoryWarning {
@@ -44,9 +87,10 @@
     // Dispose of any resources that can be recreated.
 }
 
-- (void)Button0Event
+- (void)ButtonEvent
 {
-    NSLog(@"button 0");
+    NSLog(@"0");
+    resultLabel.text = @"0";
 }
 
 @end
