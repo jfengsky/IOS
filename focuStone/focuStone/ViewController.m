ceec940d2e0b9eb4569cbafce7533bde405c0aa7
jfengsky <jfengsky@gmail.com>
2015-01-10 22:14:57 +0800

    add test

--- focuStone/focuStone/ViewController.m
+++ focuStone/focuStone/ViewController.m
@@ -16,6 +16,7 @@
 
 - (void)viewDidLoad {
     UIButton *taskFolderAddButton;
+    NSLog(@"test");
     
     // [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
