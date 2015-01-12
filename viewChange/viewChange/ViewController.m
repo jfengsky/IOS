4135ccf496e1f2032383024ad5317b01b70fc275
jfengsky <jfengsky@gmail.com>
2015-01-12 21:37:47 +0800

    简单场景切换代码

---
+++ viewChange/viewChange/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  viewChange IOS实现场景切换
+//
+//  Created by jiang feng on 15/1/12.
+//  Copyright (c) 2015年 www.ieake.com. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
