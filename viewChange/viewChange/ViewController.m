0ab7c750e5de95a90e9060f7ef9b9729ba9fc74f
jfengsky <jfengsky@gmail.com>
2015-01-12 22:09:47 +0800

    添加视图

--- viewChange/viewChange/ViewController.m
+++ viewChange/viewChange/ViewController.m
@@ -16,6 +16,22 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    UIView *view1 = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 320, 460)];
+    
+    view1.backgroundColor = [UIColor redColor];
+    
+    [self.view addSubview:view1];
+    
+    UIView *view2 = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 320, 460)];
+    
+    view2.backgroundColor = [UIColor blueColor];
+    
+    [self.view addSubview:view2];
+    
+    
+    
+    
     // Do any additional setup after loading the view, typically from a nib.
 }
 
