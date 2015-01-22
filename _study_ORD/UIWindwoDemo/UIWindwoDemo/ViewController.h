9ba1774a4f4acadbf935436bd1f7b86e493b07b9
jfengsky <jfengsky@gmail.com>
2015-01-22 22:11:10 +0800

    add UIWindow

---
+++ _study_ORD/UIWindwoDemo/UIWindwoDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UIWindwoDemo
+//
+//  Created by jiang feng on 15/1/22.
+//  Copyright (c) 2015年 www.ieake.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ _study_ORD/UIWindwoDemo/UIWindwoDemo/ViewController.m
@@ -0,0 +1,32 @@
+//
+//  ViewController.m
+//  UIWindwoDemo
+//
+//  Created by jiang feng on 15/1/22.
+//  Copyright (c) 2015年 www.ieake.com. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    UIWindow *window1 = [[UIWindow alloc] init];
+    window1.frame
+    
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
