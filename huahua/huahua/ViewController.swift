07afe02a4e022bcd6c5cda6b1ab4ee7f5020b033
姜峰 <jiang.f@ctrip.com>
2015-08-14 13:50:51 +0800

    add huahua

---
+++ huahua/huahua/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  huahua
+//
+//  Created by jiang feng on 15/8/14.
+//  Copyright (c) 2015年 www.ieake.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
