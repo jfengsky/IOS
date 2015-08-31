5f26d05e6b0b9f76f456d24f6fcf9d94ec401942
姜峰 <jiang.f@ctrip.com>
2015-08-31 16:09:56 +0800

    add playground

---
+++ learnSwift/1.playground/Contents.swift
@@ -0,0 +1,11 @@
+//: Playground - noun: a place where people can play
+
+import Foundation
+
+import UIKit
+
+let student = (name:"zhangsan", 28, true)
+println(student.name)
+println(student.1)
+
+
