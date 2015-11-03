ff3a3bc01225f2eb75fc74d83bedbcfe5df2e651
jfengsky <jfengsky@gmail.com>
2015-11-03 11:11:58 +0800

    删除mypassword

--- readJson/readJson/ViewController.swift
+++ readJson/readJson/ViewController.swift
@@ -12,6 +12,10 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        var sp = NSSearchPathForDirectoriesInDomains(NSSearchPathDirectory.DocumentationDirectory, NSSearchPathDomainMask.AllDomainsMask, true)
+        print(sp)
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
